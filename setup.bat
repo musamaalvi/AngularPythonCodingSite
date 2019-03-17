ac0783bbd00c335010a6015a8d91b491db888735
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-03-18 00:25:11 +0500

    modify setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 pip install -r PythonServer\\requirements.txt 
 pyinstaller run_service.py
 pyinstaller run_service.py -F
-
+pyinstaller PythonServer\\server.py
+pyinstaller PythonServer\\server.py -F
