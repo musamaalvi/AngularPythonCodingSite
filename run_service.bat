ea7b3effac2b2773ddc20a7c02e4b3b29fcce3d8
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-03-17 20:39:00 +0500

    improve bat

--- run_service.bat
+++ run_service.bat
@@ -1,1 +1,4 @@
-python C:\Users\user\Desktop\coding_bat_website\AngularPythonCodingSite_Usama\AngularPythonCodingSite\run_service.py
+@echo off
+set "prj_dir=C:\Users\user\Desktop\coding_bat_website\AngularPythonCodingSite_Usama\AngularPythonCodingSite" #this is where your node project lives
+set "complete_path= %prj_dir%\run_service.py"
+python %complete_path%
