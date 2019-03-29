bffec03cbd693f04dc635ac76730611e8c2822f6
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-03-29 13:02:11 +0500

    add pyinstaller in bat file

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@ cd libraries
 pip install Flask-1.0.2-py2.py3-none-any.whl
 pip install Flask_Cors-3.0.7-py2.py3-none-any.whl
 pip install Flask_RESTful-0.3.7-py2.py3-none-any.whl
+pip install PyInstaller-3.4-py2.py3-none-any.whl
 cd Flask-Jsonpify-1.5.0
 python setup.py install
 cd ..//..
