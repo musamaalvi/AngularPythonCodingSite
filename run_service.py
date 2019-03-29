519d4b9727777b1c7906c9e343deb0bb00c25f49
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-03-29 14:33:26 +0500

    change relative path to absolute path

--- run_service.py
+++ run_service.py
@@ -1,11 +1,12 @@
 from subprocess import call
-import os
+
+ABSOLUTE_PATH = 'C:\\CodingBat\\'
 
 def call_angular_service():
     '''
     calls angular service, assumes that node has already been installed on the system
     '''
-    call(['cd', os.getcwd()], shell=True) #navigate to current directory
+    call(['cd', ABSOLUTE_PATH], shell=True) #navigate to current directory
     call(['ng', 'serve'], shell=True) #run angular application
 
 
