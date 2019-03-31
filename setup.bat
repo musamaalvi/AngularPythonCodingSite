49ebdf2f48e6cd8f240002f1ca3b3ed9da38c130
FOCUS-Pakistan <aa18514.imperial@gmail.com>
2019-03-31 23:58:22 +0500

    add dependency

--- setup.bat
+++ setup.bat
@@ -8,4 +8,4 @@ python setup.py install
 cd ..//..
 pyinstaller run_service.py
 pyinstaller run_service.py -F
-npm install --save-dev @angular-devkit
+npm install --save-dev @angular-devkit/build-angular
